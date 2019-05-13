21cbcd71552a75b485c0d5a3574af79f5e7015be
Fabio Carriel Fonseca <carrielfonseca@yahoo.com.br>
2019-05-13 12:43:11 -0400

    corrects typos

--- src/toolbox/StaticMethod.java
+++ src/toolbox/StaticMethod.java
@@ -1,10 +1,10 @@
 package toolbox;
 
 // when you call a method inside another method in the same class, what you are doing implicitly is
-// using the object created to call this method. However, is this method is static, 
+// using the object created to call this method. However, if this method is static, 
 // Java will use the method inside as another static (Class reference) method/
-// This is the reason it doesn't really work call a non-static method inside a static one:
-// Jaca would try to call the method inside as a static reference, which simply does not exist
+// This is the reason it doesn't really work to call a non-static method inside a static one:
+// Java would try to call the method inside as a static reference, which simply does not exist
 
 public class StaticMethod {
 
